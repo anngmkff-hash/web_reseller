0ec6d44f6a9e4114a72cf1887d479f445ded395e
anngmkff-hash <anang.makruf241206@gmail.com>
2026-01-08 08:38:25 +0700

    Fix server start path for Railway

--- reseller-backend/server.js
+++ reseller-backend/server.js
@@ -7,6 +7,9 @@ const { Pool } = require("pg");
 
 const app = express();
 const port = process.env.PORT || 5000;
+app.listen(port, "0.0.0.0", function () {
+  // ...
+});
 
 app.use(cors());
 app.use(express.json());
